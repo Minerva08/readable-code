910094eb2d607ebc625e92b3e6f2c14c38cbf69c
Minerva08 <elavl@naver.com>
2024-10-09 23:38:20 +0900

    [StudyCafe] - FileHandler -> PassProvider 리팩토링

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,9 +1,18 @@
 package cleancode.studycafe.tobe;
 
+import cleancode.studycafe.tobe.io.provider.LockerPassFileReader;
+import cleancode.studycafe.tobe.io.provider.SeatPassFileReader;
+import cleancode.studycafe.tobe.provider.LockerPassProvider;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
+
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+
+        SeatPassFileReader seatPassFileReader = new SeatPassFileReader();
+        LockerPassFileReader lockerPassFileReader = new LockerPassFileReader();
+
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(seatPassFileReader, lockerPassFileReader);
         studyCafePassMachine.run();
     }
 
